c11b05174f7e29209848842f7d53913dae0e26ff
Eglatcova <e.glatcova@yandex.ru>
2020-03-12 23:46:05 +0300

    feat: add scroll button

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-document.addEventListener("DOMContentLoaded", function () {
+/*document.addEventListener("DOMContentLoaded", function () {
   const modal = document.querySelector('.modal')
   const modalBtn = document.querySelectorAll('[data-toggle=modal]')
   const closeBtn = document.querySelector('.modal__close')
@@ -9,4 +9,29 @@ document.addEventListener("DOMContentLoaded", function () {
     element.addEventListener('click', switchModal);
   });
   closeBtn.addEventListener('click', switchModal)
+});*/
+
+$(document).ready(function () {
+  var modal = $('.modal'),
+    modalBtn = $('[data-toggle=modal]'),
+    closeBtn = $('.modal__close');
+    scrollBtn = $('.scroll-button');
+
+  modalBtn.on('click', function () {
+    modal.toggleClass('modal--visible');
+  });
+  closeBtn.on('click', function () {
+    modal.toggleClass('modal--visible');
+  });
+
+  $(window).scroll(function () {
+    if ($(window).scrollTop() > 1000) {
+      scrollBtn.addClass('show');
+    } else {
+      scrollBtn.removeClass('show');
+    }
+  });
+  scrollBtn.on('click', function() {
+    $('html, body').animate({scrollTop:0}, 1500);
+  });
 });
