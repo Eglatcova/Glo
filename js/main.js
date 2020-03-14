756f5bfd46c9ba9534b9754705ae18a4d057b0d1
Eglatcova <e.glatcova@yandex.ru>
2020-03-14 15:36:37 +0300

    feat: add section steps and slider for section steps

--- js/main.js
+++ js/main.js
@@ -16,13 +16,14 @@ $(document).ready(function () {
     modalBtn = $('[data-toggle=modal]'),
     closeBtn = $('.modal__close');
     scrollBtn = $('.scroll-button');
-    slideOne = $('.steps-description__step--one');
-    slideTwo = $('.steps-description__step--two');
-    slideThree = $('.steps-description__step--three');
-    slideFour = $('.steps-description__step--four');
-    slideFive = $('.steps-description__step--five');
-    slideSix = $('.steps-description__step--six');
-
+    //переменные названий(имен) для слайдов "6 шагов до цели". начало
+    slideOne = $('.steps-name__step--one');
+    slideTwo = $('.steps-name__step--two');
+    slideThree = $('.steps-name__step--three');
+    slideFour = $('.steps-name__step--four');
+    slideFive = $('.steps-name__step--five');
+    slideSix = $('.steps-name__step--six');
+    //конец
   modalBtn.on('click', function () {
     modal.toggleClass('modal--visible');
   });
@@ -69,96 +70,96 @@ next.css('left', prev.width() + 10 + bullets.width() + 10)
 bullets.css('left', prev.width() + 10)
 
 //слайдер2
-  var mySwiper = new Swiper('.swiper2', {
-    loop: true,
-    pagination: {
-      el: '.pagination2',
-      type: 'bullets',
+var mySwiper = new Swiper('.swiper2', {
+  loop: true,
+  pagination: {
+    el: '.pagination2',
+    type: 'bullets',
+  },
+  navigation: {
+    nextEl: '.next2',
+    prevEl: '.prev2',
+  },
+  //переключение имени слайдов над картинками. начало
+  on: {
+    slideChange: function () {
+      //1
+      if (this.realIndex == 0)
+        slideOne.addClass('steps-name__step--visible');
+      else
+        slideOne.removeClass('steps-name__step--visible');
+      //2
+      if (this.realIndex == 1)
+        slideTwo.addClass('steps-name__step--visible');
+      else
+        slideTwo.removeClass('steps-name__step--visible');
+      //3
+      if (this.realIndex == 2)
+        slideThree.addClass('steps-name__step--visible');
+      else
+        slideThree.removeClass('steps-name__step--visible');
+      //4
+      if (this.realIndex == 3)
+        slideFour.addClass('steps-name__step--visible');
+      else
+        slideFour.removeClass('steps-name__step--visible');
+      //5
+      if (this.realIndex == 4)
+        slideFive.addClass('steps-name__step--visible');
+      else
+        slideFive.removeClass('steps-name__step--visible');
+      //6
+      if (this.realIndex == 5)
+        slideSix.addClass('steps-name__step--visible');
+      else
+        slideSix.removeClass('steps-name__step--visible');
     },
-    navigation: {
-      nextEl: '.next2',
-      prevEl: '.prev2',
-    },
-    //переключение описания слайдов над картинками. начало
-    on: {
-      slideChange: function () {
-        //1
-        if (this.realIndex == 0)
-          slideOne.addClass('visible');
-        else
-          slideOne.removeClass('visible');
-        //2
-        if (this.realIndex == 1)
-          slideTwo.addClass('visible');
-        else
-          slideTwo.removeClass('visible');
-        //3
-        if (this.realIndex == 2)
-          slideThree.addClass('visible');
-        else
-          slideThree.removeClass('visible');
-        //4
-        if (this.realIndex == 3)
-          slideFour.addClass('visible');
-        else
-          slideFour.removeClass('visible');
-        //5
-        if (this.realIndex == 4)
-          slideFive.addClass('visible');
-        else
-          slideFive.removeClass('visible');
-        //6
-        if (this.realIndex == 5)
-          slideSix.addClass('visible');
-        else
-          slideSix.removeClass('visible');
-      },
-    },
-    //конец
-  });
+  },
+  //конец
+});
 //переключение слайдов при нажатии на шаги.начало
-  slideOne .on('click', function () {
-    var mySwiper0 = document.querySelectorAll('.swiper2')[0].swiper;
-    var mySwiper1 = document.querySelectorAll('.swiper2')[1].swiper;
-    mySwiper0.slideTo(1, 1000);
-    mySwiper1.slideTo(1, 1000);
-   
-  });
-  slideTwo .on('click', function () {
-    var mySwiper0 = document.querySelectorAll('.swiper2')[0].swiper;
-    var mySwiper1 = document.querySelectorAll('.swiper2')[1].swiper;
-    mySwiper0.slideTo(2, 1000);
-    mySwiper1.slideTo(2, 1000);
-   
-  });
-  slideThree .on('click', function () {
-    var mySwiper0 = document.querySelectorAll('.swiper2')[0].swiper;
-    var mySwiper1 = document.querySelectorAll('.swiper2')[1].swiper;
-    mySwiper0.slideTo(3, 1000);
-    mySwiper1.slideTo(3, 1000);
-   
-  });
-  slideFour .on('click', function () {
-    var mySwiper0 = document.querySelectorAll('.swiper2')[0].swiper;
-    var mySwiper1 = document.querySelectorAll('.swiper2')[1].swiper;
-    mySwiper0.slideTo(4, 1000);
-    mySwiper1.slideTo(4, 1000);
-   
-  });
-  slideFive .on('click', function () {
-    var mySwiper0 = document.querySelectorAll('.swiper2')[0].swiper;
-    var mySwiper1 = document.querySelectorAll('.swiper2')[1].swiper;
-    mySwiper0.slideTo(5, 1000);
-    mySwiper1.slideTo(5, 1000);
-   
-  });
-  slideSix .on('click', function () {
-    var mySwiper0 = document.querySelectorAll('.swiper2')[0].swiper;
-    var mySwiper1 = document.querySelectorAll('.swiper2')[1].swiper;
-    mySwiper0.slideTo(6, 1000);
-    mySwiper1.slideTo(6, 1000);
-   
-  });
+slideOne .on('click', function () {
+  var mySwiper0 = document.querySelectorAll('.swiper2')[0].swiper;
+  var mySwiper1 = document.querySelectorAll('.swiper2')[1].swiper;
+  mySwiper0.slideTo(1, 1000);
+  mySwiper1.slideTo(1, 1000);
+  
+});
+slideTwo .on('click', function () {
+  var mySwiper0 = document.querySelectorAll('.swiper2')[0].swiper;
+  var mySwiper1 = document.querySelectorAll('.swiper2')[1].swiper;
+  mySwiper0.slideTo(2, 1000);
+  mySwiper1.slideTo(2, 1000);
+  
+});
+slideThree .on('click', function () {
+  var mySwiper0 = document.querySelectorAll('.swiper2')[0].swiper;
+  var mySwiper1 = document.querySelectorAll('.swiper2')[1].swiper;
+  mySwiper0.slideTo(3, 1000);
+  mySwiper1.slideTo(3, 1000);
+  
+});
+slideFour .on('click', function () {
+  var mySwiper0 = document.querySelectorAll('.swiper2')[0].swiper;
+  var mySwiper1 = document.querySelectorAll('.swiper2')[1].swiper;
+  mySwiper0.slideTo(4, 1000);
+  mySwiper1.slideTo(4, 1000);
+  
+});
+slideFive .on('click', function () {
+  var mySwiper0 = document.querySelectorAll('.swiper2')[0].swiper;
+  var mySwiper1 = document.querySelectorAll('.swiper2')[1].swiper;
+  mySwiper0.slideTo(5, 1000);
+  mySwiper1.slideTo(5, 1000);
+  
+});
+slideSix .on('click', function () {
+  var mySwiper0 = document.querySelectorAll('.swiper2')[0].swiper;
+  var mySwiper1 = document.querySelectorAll('.swiper2')[1].swiper;
+  mySwiper0.slideTo(6, 1000);
+  mySwiper1.slideTo(6, 1000);
+  
+});
 //конец
 
 
